5e7cb3c8527c302a1c4414f19d093ee015762cb8
rachataptnn <rachata.ptnn@gmail.com>
2025-03-27 17:23:05 +0700

    i t s w o r k e d

--- main.go
+++ main.go
@@ -3,12 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
 )
 
 func main() {
-	prepareWordCategories()
+	wcs, err := prepareWordCategories()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	hangman(wcs)
 }
 
 type WordCategory struct {
@@ -74,4 +81,115 @@ func prepareWordCategory(fileName string) (WordCategory, error) {
 	}
 
 	return category, nil
+}
+
+//
+
+func hangman(wcs []WordCategory) {
+	categoryNumber := selectCategory(wcs)
+
+	category := wcs[categoryNumber-1]
+	word := category.Words[rand.Intn(len(category.Words))]
+
+	fmt.Println("Hint: ", word.Hint)
+	fmt.Println("")
+
+	isWin, score := guess(word)
+	if isWin {
+		fmt.Println("Congratulations! You found the word!")
+		fmt.Println("your score: ", score)
+	}
+
+}
+
+func selectCategory(wcs []WordCategory) int {
+	fmt.Println("Select Category:")
+	for i, wc := range wcs {
+		fmt.Printf("%d. %s\n", i+1, wc.Name)
+	}
+	fmt.Println("")
+
+	var categoryNumber int
+	for categoryNumber <= 0 || categoryNumber > len(wcs) {
+		fmt.Print("Enter Category Number: ")
+		fmt.Scanln(&categoryNumber)
+
+		if categoryNumber < 0 || categoryNumber > len(wcs) {
+			fmt.Println("Invalid Category Number")
+		}
+	}
+
+	return categoryNumber
+}
+
+func guess(word word) (isPass bool, score int) {
+	incorrectGuessLimit := 10
+
+	process := ""
+	n := len(word.Word)
+	for _, v := range word.Word {
+		if v == ' ' {
+			process += " "
+		} else {
+			process += "_"
+		}
+
+	}
+	showProcess(process, n, incorrectGuessLimit)
+
+	correctCount := 0
+	for incorrectGuessLimit > 0 && correctCount != n {
+		var letter string
+		fmt.Print("Enter a letter: ")
+		fmt.Scanln(&letter)
+
+		if len(letter) != 1 {
+			fmt.Println("")
+			fmt.Println("Invalid Letter")
+			fmt.Println("")
+			continue
+		}
+
+		if strings.Contains(word.Word, letter) {
+			fmt.Println("")
+			fmt.Println("Correct Letter!")
+			fmt.Println("")
+
+			correctCount += 1
+			score += 10
+			process = updateProcess(word.Word, process, letter, n)
+			showProcess(process, score, incorrectGuessLimit)
+
+		} else {
+			fmt.Println("")
+			fmt.Println("Incorrect Letter~")
+			fmt.Println("")
+
+			incorrectGuessLimit--
+			showProcess(process, score, incorrectGuessLimit)
+
+			if incorrectGuessLimit == 0 {
+				fmt.Println("Game Over!")
+				fmt.Println("Word: ", word.Word)
+				return false, score
+			}
+		}
+	}
+
+	return true, score
+}
+
+func showProcess(process string, score, incorrectGuessLimit int) {
+	fmt.Printf("%s	score:%d remaining incorrect guess:%d\n", process, score, incorrectGuessLimit)
+}
+
+func updateProcess(word, process, letter string, n int) string {
+	for i := 0; i < n; i++ {
+		if word[i] == letter[0] && process[i] == '_' {
+			process = process[:i] + string(letter) + process[i+1:]
+			break
+		}
+	}
+
+	return process
 }
